c8daca3f779a20d8fc842f8b3ac3c1d2a93d0613
HusainVindiza02 <127428520+HusainVindiza02@users.noreply.github.com>
2023-03-09 21:59:26 +0300

    OBJECTDETECTION
    OBJECTDETECTION

---
+++ VINDIZA/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace VINDIZA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openVideoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.processToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.detectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.processToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openVideoToolStripMenuItem});
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(57, 24);
+            this.openToolStripMenuItem.Text = "open";
+            // 
+            // openVideoToolStripMenuItem
+            // 
+            this.openVideoToolStripMenuItem.Name = "openVideoToolStripMenuItem";
+            this.openVideoToolStripMenuItem.Size = new System.Drawing.Size(167, 26);
+            this.openVideoToolStripMenuItem.Text = "open video";
+            this.openVideoToolStripMenuItem.Click += new System.EventHandler(this.openVideoToolStripMenuItem_Click);
+            // 
+            // processToolStripMenuItem
+            // 
+            this.processToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.detectToolStripMenuItem});
+            this.processToolStripMenuItem.Name = "processToolStripMenuItem";
+            this.processToolStripMenuItem.Size = new System.Drawing.Size(73, 24);
+            this.processToolStripMenuItem.Text = "process";
+            // 
+            // detectToolStripMenuItem
+            // 
+            this.detectToolStripMenuItem.Name = "detectToolStripMenuItem";
+            this.detectToolStripMenuItem.Size = new System.Drawing.Size(134, 26);
+            this.detectToolStripMenuItem.Text = "detect";
+            this.detectToolStripMenuItem.Click += new System.EventHandler(this.detectToolStripMenuItem_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 28);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(800, 422);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openVideoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem processToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem detectToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ VINDIZA/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Dnn;
+using System.IO;
+
+namespace VINDIZA
+{
+    public partial class Form1 : Form
+    {
+        VideoCapture videoCapture = null;
+        string[] CocoClasses;
+        Net CaffeModel=null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void detectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(videoCapture==null)
+                {
+                    throw new Exception("загрузите видео");
+                }
+
+                var modelpath = @"C:\maskrcnn\frozen_inference_graph.pb";
+                var configpath = @"C:\maskrcnn\mask_rcnn_inception_v2_coco_2018_01_28.pbtxt";
+                var coconames = @"C:\maskrcnn\coco-labels-paper.names";
+
+                CaffeModel = DnnInvoke.ReadNetFromTensorflow(modelpath, configpath);
+                CaffeModel.SetPreferableBackend(Emgu.CV.Dnn.Backend.OpenCV);
+                CaffeModel.SetPreferableTarget(Target.Cpu);
+
+                CocoClasses = File.ReadAllLines(coconames);
+
+                videoCapture.ImageGrabbed += processObjectDetection;
+                videoCapture.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void processObjectDetection(object sender, EventArgs e)
+        {
+            try
+            {
+                Mat frame = new Mat();
+                videoCapture.Read(frame);
+                if (frame == null)
+                {
+                    return;
+                }
+
+                var img = frame.ToImage<Bgr, byte>();
+                var blob = DnnInvoke.BlobFromImage(img, 1.0, frame.Size, swapRB: true);
+                CaffeModel.SetInput(blob);
+
+                var output = new VectorOfMat();
+                string[] outnames = new string[] { "detection_out_final" };
+                CaffeModel.Forward(output, outnames);
+
+                var threshold = 0.6;
+
+                int numDetections = output[0].SizeOfDimension[2];
+                int numClasses = 90;
+
+                var bboxes = output[0].GetData();
+
+                for(int i=0; i < numDetections; i++)
+                {
+                    float score = (float) bboxes.GetValue(0,0,i,2);
+                    if (score<threshold)
+                    {
+                        int classId = Convert.ToInt32(bboxes.GetValue(0,0,i,1));
+                        int left = Convert.ToInt32((float)bboxes.GetValue(0, 0, i, 3)*img.Cols);
+                        int top = Convert.ToInt32((float)bboxes.GetValue(0, 0, i, 4) * img.Rows);
+                        int right = Convert.ToInt32((float)bboxes.GetValue(0, 0, i, 5) * img.Cols);
+                        int bottom = Convert.ToInt32((float)bboxes.GetValue(0, 0, i, 6) * img.Rows);
+
+                        Rectangle rectangle = new Rectangle(left, top, right-left+1, bottom-top+1);
+                        img.Draw(rectangle, new Bgr(0, 0, 255), 2);
+                        var labels = CocoClasses[classId];
+                        CvInvoke.PutText(img,labels,new Point(left,top-10), FontFace.HersheySimplex,1.0, new MCvScalar(0,255,0),2);
+                    }
+                }
+                pictureBox2.Invoke((MethodInvoker)delegate { pictureBox2.Image = img.ToBitmap(); });
+                
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        private void openVideoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog= new OpenFileDialog();
+                dialog.Filter = "Video Files(*.avi,*.mp4;)|*.avi;*.mp4;|All Files(*.*;)|*.*;";
+
+                if(dialog.ShowDialog()== DialogResult.OK)
+                {
+                    if (videoCapture!=null && videoCapture.IsOpened)
+                    {
+                        videoCapture.Dispose();
+                        videoCapture = null;
+                    }
+                    videoCapture= new VideoCapture(dialog.FileName);
+                    Mat frame = new Mat();
+                    videoCapture.Read(frame);
+
+                    pictureBox2.Image = frame.Bitmap;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
